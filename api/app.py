51ade546c04437e3cc030519bfd486a7b10f3376
Piotr Persona <persona.piotr@gmail.com>
2020-05-27 20:24:58 +0200

    Create STEVE meeting <3
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- api/app.py
+++ api/app.py
@@ -5,12 +5,20 @@ from swagger_ui_bundle import swagger_ui_3_path
 
 from api.environment import Environment
 from api.libs.db.mongo import MongoStore
+from api.libs.google.google_client import GoogleClient
+from api.libs.token.steve import SteveTokenService
+from api.meeting.google_scheduler import GoogleScheduler
 
 logging.basicConfig(level=logging.DEBUG)
 env = Environment.read()
 
 mongo_client = pymongo.MongoClient(env.mongo.uri, username=env.mongo.user, password=env.mongo.password)
 store = MongoStore(mongo_client, env.mongo.database, env.mongo.collection)
+
+google_client = GoogleClient(env.google.host)
+meeting_scheduler = GoogleScheduler(google_client)
+token_service = SteveTokenService(env.auth.url)
+
 
 def main():
     options = {"swagger_path": swagger_ui_3_path}
--- api/environment.py
+++ api/environment.py
@@ -13,10 +13,24 @@ class Server(EnvironmentReader):
         self.port = self.get('port')
 
 
+class Google(EnvironmentReader):
+    def __init__(self):
+        super()
+        self.host = self.get('host')
+
+
+class Auth(EnvironmentReader):
+    def __init__(self):
+        super()
+        self.url = self.get('url')
+
+
 class Environment(PrettyPrint):
     def __init__(self):
         self.server = Server()
         self.mongo = Mongo()
+        self.google = Google()
+        self.auth = Auth()
 
     @staticmethod
     def read():
